8a808589ab8d43bd3f63af5511d3f385a1b2292c
Julia <us.julija@gmail.com>
2018-06-30 16:43:28 +0300

    readme added

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ var version = 'v1.0';
 self.addEventListener("install", function(event) {
   console.log('SWORKER: install event in progress.');
   event.waitUntil(
-    caches.open(version + 'fundamentals').then(function(cache) {
+    caches.open('fundamentals' + version).then(function(cache) {
         return cache.addAll([
           '/',
           '/css/styles.css',
@@ -32,11 +32,27 @@ self.addEventListener("install", function(event) {
   );
 });
 
+//makes it work offline
 self.addEventListener('fetch', function(event) {
   console.log(event.request.url);
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
+    })
+  );
+});
+
+//deletes previous cache (if any)
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('fundamentals') && cacheName != version;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
     })
   );
 });
