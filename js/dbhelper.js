da071d0ea0bc57cfa0861df34db51c5315352fd3
Julia <34662663+usjulija@users.noreply.github.com>
2018-07-26 16:51:29 +0300

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -8,8 +8,11 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000 // Change this to your server port
-    return './data/restaurants.json';
+    //uncoment if you use local server
+    //const port = 8000 //Change this to your server port
+    //return `http://localhost:${port}/data/restaurants.json`;
+    //delete the below if you use locl server
+    return '//usjulija.github.io/restaurantApp/data/restaurants.json';
   }
 
   /**
@@ -150,7 +153,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`./img/${restaurant.photograph}`);
+    return (`//usjulija.github.io/restaurantApp/img/${restaurant.photograph}`);
   }
 
   /**
